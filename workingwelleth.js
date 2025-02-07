f39a5e553713fc091e263170b77482f2fb09a966
beyondmoonmedia <dev@beyondmoon.media>
2025-02-07 19:11:06 +0800

    updates

--- workingwelleth.js
+++ workingwelleth.js
@@ -491,12 +491,9 @@ app.post('/webhook/bsc/transactions', async (req, res) => {
     try {
         console.log(req.body)
         // Check if it's an address activity webhook
-        console.log(req.body.event.data.block)
         // if (req.body.type !== 'ADDRESS_ACTIVITY') {
         //     return res.status(200).json({ message: 'Not an address activity event' });
         // }
-        console.log(req.body.event.data.block)
-        console.log(req.body.event.activity[0].hash)
 
         const activities = req.body.event.activity;
         if (!Array.isArray(activities)) {
